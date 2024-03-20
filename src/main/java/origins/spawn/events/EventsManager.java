1d7019b63cb426e277051ab89c59c8cd8001ca08
Leonardo Barros <62991452+leonardobarrosbrasil@users.noreply.github.com>
2024-03-20 17:00:51 -0300

    xxxx

--- src/main/java/origins/spawn/events/EventsManager.java
+++ src/main/java/origins/spawn/events/EventsManager.java
@@ -15,97 +15,103 @@ import java.util.UUID;
 
 public class EventsManager implements Listener {
 
-  @EventHandler
-  public void onRespawn(PlayerRespawnEvent event) {
-    event.setRespawnLocation(MainSpawn.getPlugin().getFunctions().getSpawn());
-  }
+   @EventHandler
+   public void onRespawn(PlayerRespawnEvent event) {
+      event.setRespawnLocation(MainSpawn.getPlugin().getFunctions().getSpawn());
+   }
 
-  @EventHandler
-  public void onMove(PlayerMoveEvent event) {
-    Player player = event.getPlayer();
-    HashMap<UUID, Location> cooldowns = MainSpawn.getPlugin().teleportCooldown;
+   @EventHandler
+   public void onMove(PlayerMoveEvent event) {
+      Player player = event.getPlayer();
+      HashMap<UUID, Location> cooldowns = MainSpawn.getPlugin().teleportCooldown;
 
-    if(cooldowns.containsKey(player.getUniqueId())) {
-      Location initialLocation = cooldowns.get(player.getUniqueId());
-      Location currentLocation = event.getTo();
+      if (cooldowns.containsKey(player.getUniqueId())) {
+         Location initialLocation = cooldowns.get(player.getUniqueId());
+         Location currentLocation = event.getTo();
 
-      if(currentLocation != null) {
-        double initialX = initialLocation.getX();
-        double initialZ = initialLocation.getZ();
+         if (currentLocation != null) {
+            double initialX = initialLocation.getX();
+            double initialZ = initialLocation.getZ();
 
-        double currentX = currentLocation.getX();
-        double currentZ = currentLocation.getZ();
+            double currentX = currentLocation.getX();
+            double currentZ = currentLocation.getZ();
 
-        if (initialX != currentX || initialZ != currentZ) {
-          player.sendMessage("§cTeletransporte cancelado.");
-          player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1.0F, 1.0F);
-          cooldowns.remove(player.getUniqueId());
-        }
+            if (initialX != currentX || initialZ != currentZ) {
+               player.sendMessage("§cTeletransporte cancelado.");
+               player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1.0F, 1.0F);
+               cooldowns.remove(player.getUniqueId());
+            }
+         }
       }
-    }
-  }
+   }
 
-  @EventHandler
-  public void onJoin(PlayerJoinEvent event) {
-    Player player = event.getPlayer();
+   @EventHandler
+   public void onJoin(PlayerJoinEvent event) {
+      Player player = event.getPlayer();
 
-    if (!player.hasPlayedBefore()) {
-      // Teleportar o jogador para o inicio
-      Bukkit.getScheduler().runTaskLater(MainSpawn.getPlugin(), () -> player.teleport(MainSpawn.getPlugin().getFunctions().getSpawn(), PlayerTeleportEvent.TeleportCause.COMMAND), 3L);
+      if (!player.hasPlayedBefore()) {
+         // Teleportar o jogador para o inicio
+         Bukkit.getScheduler().runTaskLater(MainSpawn.getPlugin(), () -> player.teleport(MainSpawn.getPlugin().getFunctions().getSpawn(), PlayerTeleportEvent.TeleportCause.COMMAND), 3L);
 
-      // Sistema de enviar mensagem
-      boolean messageStatus = MainSpawn.getPlugin().getConfig().getBoolean("welcomeMessageStatus"); // Estado da função de mensagem de boas vindas.
+         // Sistema de enviar mensagem
+         boolean messageStatus = MainSpawn.getPlugin().getConfig().getBoolean("welcomeMessageStatus"); // Estado da função de mensagem de boas vindas.
 
-      String defaultMessage = "§a%player% entrou pela primeira vez no servidor.";
-      String message = MainSpawn.getPlugin().getConfig().getString("welcomeMessage");
-      message = (message != null) ? message : defaultMessage;
+         String defaultMessage = "§a%player% entrou pela primeira vez no servidor.";
+         String message = MainSpawn.getPlugin().getConfig().getString("welcomeMessage");
+         message = (message != null) ? message : defaultMessage;
 
-      if (messageStatus) {
-        event.setJoinMessage(null);
-        Bukkit.broadcastMessage(MainSpawn.getPlugin().getFunctions().hex(message.replaceAll("%player%", player.getName())));
-        player.playSound(player.getLocation(), Sound.ENTITY_ITEM_PICKUP, 1.0F, 1.0F);
-      }
+         if (messageStatus) {
+            event.setJoinMessage(null);
+            Bukkit.broadcastMessage(MainSpawn.getPlugin().getFunctions().hex(message.replaceAll("%player%", player.getName())));
+            player.playSound(player.getLocation(), Sound.ENTITY_ITEM_PICKUP, 1.0F, 1.0F);
+         }
 
-      // Sistema de dar o conjunto
-      boolean kitStatus = MainSpawn.getPlugin().getConfig().getBoolean("kitStatus");
-      String kitName = MainSpawn.getPlugin().getConfig().getString("kitName");
+         // Sistema de dar o conjunto
+         boolean kitStatus = MainSpawn.getPlugin().getConfig().getBoolean("kitStatus");
+         String kitName = MainSpawn.getPlugin().getConfig().getString("kitName");
 
-      if (kitStatus && kitName != null) {
-        MainKits.getPlugin().getFunctions().giveKit(player, kitName, false);
-      }
-    } else {
-      boolean joinMessageStatus = MainSpawn.getPlugin().getConfig().getBoolean("joinMessageStatus");
+         if (kitStatus && kitName != null) {
+            MainKits.getPlugin().getFunctions().giveKit(player, kitName, false);
+         }
 
-      String defaultMessage = "§a%player% entrou no servidor";
-      String joinMessage = MainSpawn.getPlugin().getConfig().getString("joinMessage");
-      joinMessage = (joinMessage != null) ? joinMessage : defaultMessage;
+         boolean fireworkStatus = MainSpawn.getPlugin().getConfig().getBoolean("fireworkStatus");
 
-      if (joinMessageStatus) {
-        event.setJoinMessage(MainSpawn.getPlugin().getFunctions().hex(joinMessage.replaceAll("%player%", player.getName())));
+         if (fireworkStatus) {
+            MainSpawn.getPlugin().getFunctions().launchRandomFirework(player);
+         }
       } else {
-        event.setJoinMessage(null);
+         boolean joinMessageStatus = MainSpawn.getPlugin().getConfig().getBoolean("joinMessageStatus");
+
+         String defaultMessage = "§a%player% entrou no servidor";
+         String joinMessage = MainSpawn.getPlugin().getConfig().getString("joinMessage");
+         joinMessage = (joinMessage != null) ? joinMessage : defaultMessage;
+
+         if (joinMessageStatus) {
+            event.setJoinMessage(MainSpawn.getPlugin().getFunctions().hex(joinMessage.replaceAll("%player%", player.getName())));
+         } else {
+            event.setJoinMessage(null);
+         }
       }
-    }
-  }
+   }
 
-  @EventHandler
-  public void onLeave(PlayerQuitEvent event) {
-    Player player = event.getPlayer();
-    HashMap<UUID, Location> cooldowns = MainSpawn.getPlugin().teleportCooldown;
+   @EventHandler
+   public void onLeave(PlayerQuitEvent event) {
+      Player player = event.getPlayer();
+      HashMap<UUID, Location> cooldowns = MainSpawn.getPlugin().teleportCooldown;
 
-    // Remover o jogador do teletransporte caso esteja
-    cooldowns.remove(player.getUniqueId());
+      // Remover o jogador do teletransporte caso esteja
+      cooldowns.remove(player.getUniqueId());
 
-    boolean quitMessageStatus = MainSpawn.getPlugin().getConfig().getBoolean("quitMessageStatus");
+      boolean quitMessageStatus = MainSpawn.getPlugin().getConfig().getBoolean("quitMessageStatus");
 
-    String defaultMessage = "§c%player% saiu do servidor.";
-    String quitMessage = MainSpawn.getPlugin().getConfig().getString("quitMessage");
-    quitMessage = (quitMessage != null) ? quitMessage : defaultMessage;
+      String defaultMessage = "§c%player% saiu do servidor.";
+      String quitMessage = MainSpawn.getPlugin().getConfig().getString("quitMessage");
+      quitMessage = (quitMessage != null) ? quitMessage : defaultMessage;
 
-    if (quitMessageStatus){
-      event.setQuitMessage(MainSpawn.getPlugin().getFunctions().hex(quitMessage.replaceAll("%player%", player.getName())));
-    } else {
-      event.setQuitMessage(null);
-    }
-  }
+      if (quitMessageStatus) {
+         event.setQuitMessage(MainSpawn.getPlugin().getFunctions().hex(quitMessage.replaceAll("%player%", player.getName())));
+      } else {
+         event.setQuitMessage(null);
+      }
+   }
 }
--- src/main/java/origins/spawn/utils/FunctionsManager.java
+++ src/main/java/origins/spawn/utils/FunctionsManager.java
@@ -1,146 +1,176 @@
 package origins.spawn.utils;
 
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Sound;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 import origins.spawn.main.MainSpawn;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FunctionsManager {
 
-  public FileConfiguration getConfig() {
-    return MainSpawn.getPlugin().getConfig();
-  }
+   public FileConfiguration getConfig() {
+      return MainSpawn.getPlugin().getConfig();
+   }
 
-  public void teleportSpawn(Player player) {
-    UUID uuid = player.getUniqueId();
-    HashMap<UUID, Location> cooldowns = MainSpawn.getPlugin().teleportCooldown;
+   public void teleportSpawn(Player player) {
+      UUID uuid = player.getUniqueId();
+      HashMap<UUID, Location> cooldowns = MainSpawn.getPlugin().teleportCooldown;
 
-    if (cooldowns.containsKey(uuid)) {
-      player.sendMessage("§cVocê já está em um teleporte.");
-      return;
-    }
-    if (getSpawn() == null) {
-      player.sendMessage("§cLocal de origem não encontrado.");
-      return;
-    }
-    if(player.hasPermission("origins.spawn.bypass")) {
-      player.teleport(getSpawn());
-      player.sendMessage("§aTeletransportado para o inicio.");
-      player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
-      return;
-    }
-
-    cooldowns.put(uuid, player.getLocation());
-    new BukkitRunnable() {
-      int countdown = 3;
-
-      @Override
-      public void run() {
-        if (cooldowns.containsKey(uuid)) {
-          if (countdown > 0) {
-            player.sendMessage("§aTeletransportando em " + countdown + " segundo(s). Por favor, não se mova.");
-            player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1.0F, 1.0F);
-            countdown--;
-          } else {
-            player.teleport(getSpawn());
-            player.sendMessage("§aTeletransportado para o inicio.");
-            player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
-            cooldowns.remove(uuid);
-            cancel();
-          }
-        } else {
-          cooldowns.remove(uuid);
-          cancel();
-        }
+      if (cooldowns.containsKey(uuid)) {
+         player.sendMessage("§cVocê já está em um teleporte.");
+         return;
       }
-    }.runTaskTimer(MainSpawn.getPlugin(), 0L, 20);
-  }
-
-  public Location getSpawn() {
-    double x = getConfig().getDouble("spawn.coordinates.x");
-    double y = getConfig().getDouble("spawn.coordinates.y");
-    double z = getConfig().getDouble("spawn.coordinates.z");
-    float yaw = (float) getConfig().getDouble("spawn.coordinates.yaw");
-    float pitch = (float) getConfig().getDouble("spawn.coordinates.pitch");
-    String worldName = getConfig().getString("spawn.coordinates.world");
-    World world = Bukkit.getWorld(Objects.requireNonNull(worldName));
-
-    try {
-      final Location loc = new Location(world, x, y, z);
-      loc.setYaw(yaw);
-      loc.setPitch(pitch);
-      return loc;
-    } catch (NullPointerException exception) {
-      return null;
-    }
-  }
-
-  public void setSpawn(Player player) {
-    Location loc = player.getLocation();
-    String world = Objects.requireNonNull(loc.getWorld()).getName();
-    double x = loc.getX();
-    double y = loc.getY();
-    double z = loc.getZ();
-    float yaw = loc.getYaw();
-    float pitch = loc.getPitch();
-
-    getConfig().set("spawn.coordinates.x", x);
-    getConfig().set("spawn.coordinates.y", y);
-    getConfig().set("spawn.coordinates.z", z);
-    getConfig().set("spawn.coordinates.yaw", yaw);
-    getConfig().set("spawn.coordinates.pitch", pitch);
-    getConfig().set("spawn.coordinates.world", world);
-
-    try {
-      MainSpawn.getPlugin().saveConfig();
-      player.sendMessage("§aLocal de origem definido.");
-    } catch (Exception exception) {
-      player.sendMessage("§cOcorreu um erro ao definir o spawn.");
-    }
-  }
-
-  public void deleteSpawn(CommandSender sender) {
-    if (getSpawn() == null) {
-      sender.sendMessage("§cLocal de origem não encontrado.");
-      return;
-    }
-    try {
-      getConfig().set("spawn", null);
-      MainSpawn.getPlugin().saveConfig();
-      sender.sendMessage("§aLocal de origem removido");
-    } catch (Exception exception) {
-      sender.sendMessage("§aOcorreu um erro ao remover o local de origem.");
-    }
-  }
-
-  public String hex(String message) {
-    Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
-    Matcher matcher = pattern.matcher(message);
-    while (matcher.find()) {
-      String hexCode = message.substring(matcher.start(), matcher.end());
-      String replaceSharp = hexCode.replace('#', 'x');
-
-      char[] ch = replaceSharp.toCharArray();
-      StringBuilder builder = new StringBuilder();
-      for (char c : ch) {
-        builder.append("&").append(c);
+      if (getSpawn() == null) {
+         player.sendMessage("§cLocal de origem não encontrado.");
+         return;
       }
-      message = message.replace(hexCode, builder.toString());
-      matcher = pattern.matcher(message);
-    }
-    return ChatColor.translateAlternateColorCodes('&', message);
-  }
+      if (player.hasPermission("origins.spawn.bypass")) {
+         player.teleport(getSpawn());
+         player.sendMessage("§aTeletransportado para o inicio.");
+         player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
+         return;
+      }
+
+      cooldowns.put(uuid, player.getLocation());
+      new BukkitRunnable() {
+         int countdown = 3;
+
+         @Override
+         public void run() {
+            if (cooldowns.containsKey(uuid)) {
+               if (countdown > 0) {
+                  player.sendMessage("§aTeletransportando em " + countdown + " segundo(s). Por favor, não se mova.");
+                  player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1.0F, 1.0F);
+                  countdown--;
+               } else {
+                  player.teleport(getSpawn());
+                  player.sendMessage("§aTeletransportado para o inicio.");
+                  player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
+                  cooldowns.remove(uuid);
+                  cancel();
+               }
+            } else {
+               cooldowns.remove(uuid);
+               cancel();
+            }
+         }
+      }.runTaskTimer(MainSpawn.getPlugin(), 0L, 20);
+   }
+
+   public Location getSpawn() {
+      double x = getConfig().getDouble("spawn.coordinates.x");
+      double y = getConfig().getDouble("spawn.coordinates.y");
+      double z = getConfig().getDouble("spawn.coordinates.z");
+      float yaw = (float) getConfig().getDouble("spawn.coordinates.yaw");
+      float pitch = (float) getConfig().getDouble("spawn.coordinates.pitch");
+      String worldName = getConfig().getString("spawn.coordinates.world");
+      World world = Bukkit.getWorld(Objects.requireNonNull(worldName));
+
+      try {
+         final Location loc = new Location(world, x, y, z);
+         loc.setYaw(yaw);
+         loc.setPitch(pitch);
+         return loc;
+      } catch (NullPointerException exception) {
+         return null;
+      }
+   }
+
+   public void setSpawn(Player player) {
+      Location loc = player.getLocation();
+      String world = Objects.requireNonNull(loc.getWorld()).getName();
+      double x = loc.getX();
+      double y = loc.getY();
+      double z = loc.getZ();
+      float yaw = loc.getYaw();
+      float pitch = loc.getPitch();
+
+      getConfig().set("spawn.coordinates.x", x);
+      getConfig().set("spawn.coordinates.y", y);
+      getConfig().set("spawn.coordinates.z", z);
+      getConfig().set("spawn.coordinates.yaw", yaw);
+      getConfig().set("spawn.coordinates.pitch", pitch);
+      getConfig().set("spawn.coordinates.world", world);
+
+      try {
+         MainSpawn.getPlugin().saveConfig();
+         player.sendMessage("§aLocal de origem definido.");
+      } catch (Exception exception) {
+         player.sendMessage("§cOcorreu um erro ao definir o spawn.");
+      }
+   }
+
+   public void deleteSpawn(CommandSender sender) {
+      if (getSpawn() == null) {
+         sender.sendMessage("§cLocal de origem não encontrado.");
+         return;
+      }
+      try {
+         getConfig().set("spawn", null);
+         MainSpawn.getPlugin().saveConfig();
+         sender.sendMessage("§aLocal de origem removido");
+      } catch (Exception exception) {
+         sender.sendMessage("§aOcorreu um erro ao remover o local de origem.");
+      }
+   }
+
+   public String hex(String message) {
+      Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
+      Matcher matcher = pattern.matcher(message);
+      while (matcher.find()) {
+         String hexCode = message.substring(matcher.start(), matcher.end());
+         String replaceSharp = hexCode.replace('#', 'x');
+
+         char[] ch = replaceSharp.toCharArray();
+         StringBuilder builder = new StringBuilder();
+         for (char c : ch) {
+            builder.append("&").append(c);
+         }
+         message = message.replace(hexCode, builder.toString());
+         matcher = pattern.matcher(message);
+      }
+      return ChatColor.translateAlternateColorCodes('&', message);
+   }
+
+   public void launchRandomFirework(Player player) {
+      Firework firework = (Firework) Objects.requireNonNull(player.getWorld()).spawnEntity(player.getLocation(), EntityType.FIREWORK);
+      FireworkMeta fireworkMeta = firework.getFireworkMeta();
+      Random random = new Random();
+      FireworkEffect effect = FireworkEffect.builder().flicker(random.nextBoolean()).withColor(Objects.requireNonNull(getColor(random.nextInt(17) + 1))).withFade(Objects.requireNonNull(getColor(random.nextInt(17) + 1))).with(FireworkEffect.Type.values()[random.nextInt((FireworkEffect.Type.values()).length)]).trail(random.nextBoolean()).build();
+      fireworkMeta.addEffect(effect);
+      fireworkMeta.setPower(random.nextInt(2) + 1);
+      firework.setFireworkMeta(fireworkMeta);
+   }
+
+   private Color getColor(int i) {
+      return switch (i) {
+         case 1 -> Color.AQUA;
+         case 2 -> Color.BLACK;
+         case 3 -> Color.BLUE;
+         case 4 -> Color.FUCHSIA;
+         case 5 -> Color.GRAY;
+         case 6 -> Color.GREEN;
+         case 7 -> Color.LIME;
+         case 8 -> Color.MAROON;
+         case 9 -> Color.NAVY;
+         case 10 -> Color.OLIVE;
+         case 11 -> Color.ORANGE;
+         case 12 -> Color.PURPLE;
+         case 13 -> Color.RED;
+         case 14 -> Color.SILVER;
+         case 15 -> Color.TEAL;
+         case 16 -> Color.WHITE;
+         case 17 -> Color.YELLOW;
+         default -> null;
+      };
+   }
 }
